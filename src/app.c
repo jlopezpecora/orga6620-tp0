204c600e9e8e129b06c4b238359fdf5f02a3baf7
Agustin Linari <agustinlinari@gmail.com>
2016-09-05 21:59:18 -0300

    graficador de fractal julia

--- src/app.c
+++ src/app.c
@@ -8,6 +8,7 @@
 
 static void print_help();
 static void print_version();
+static void drawJulia(int x, int y, double w, double h, complex_t zc, complex_t c,int N);
 
 const char* short_options = "hV";
 
@@ -33,8 +34,19 @@ int main(int argc, char** argv) {
 				break;
 		}
 	} else {
-        
-    }
+		complex_t zc;
+		zc.re = 10;
+		zc.im = 5;
+		complex_t c;
+		c.re = 10;
+		c.im = 2;
+		int x = 100;	
+		int y = 100;
+		double w = 1000;
+		double h = 1000;
+             	int N = 255;
+        	drawJulia(x,y,w,h,zc,c,N);
+   	}
     return EXIT_SUCCESS;
 }
 
@@ -57,4 +69,42 @@ static void print_help() {
            "  tp0 < in.txt > out.txt\n"
            "  cat in.txt | tp0 > out.txt\n");
 }
+
+static void pixelToComplex(complex_t* z, int i, int j, double dw, double dh, double re0, double im0){
+         
+	double re = re0+dw/2+dw*i;
+        double im = im0-dh/2-dh*j;
+	complex_create(z,re,im);
+}
+
+static void drawJulia(int x, int y, double w, double h, complex_t zc, complex_t c,int N){
+	FILE* pFile;
+	pFile = fopen("julia.gpg","w");
+	double dw=w/x;
+	double dh = h/y;
+	double re0 = zc.re - w/2;
+	double im0 = zc.im + h/2;
+
+	for (int j=0; j<y; ++j){
+	    for (int i =0; i<x; ++i){
+		complex_t* z = malloc(sizeof(complex_t));            
+    		pixelToComplex(z, i, j, dw, dh, re0, im0);
+	        int brillo = N;
+	        for (int k=0; k<N-1;++k){
+	        	if (complex_abs(z)>2){
+		            brillo = k;
+		            break;
+			}
+			complex_mult(z,*z);
+			complex_add(z,c);
+		}
+		fprintf(pFile,"%d",brillo);
+		free(z);
+	    }
+	    fprintf(pFile,"\n");
+	}
+	fclose(pFile);
+}
+
+
 
